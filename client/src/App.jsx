bc2ff483c2f1611f22215df4fbb638c2aaa33246
Aman Kumar <amankumar3432k@gmail.com>
2026-06-04 12:23:01 +0530

    Use HashRouter for GitHub Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { AnimatePresence } from 'framer-motion';
 import MainLayout from './layouts/MainLayout';
@@ -9,15 +9,15 @@ import WhatsAppButton from './components/WhatsAppButton';
 import LoadingScreen from './components/LoadingScreen';
 
 // Lazy-loaded pages
-const Home       = React.lazy(() => import('./pages/Home'));
-const About      = React.lazy(() => import('./pages/About'));
-const Menu       = React.lazy(() => import('./pages/Menu'));
-const Gallery    = React.lazy(() => import('./pages/Gallery'));
+const Home = React.lazy(() => import('./pages/Home'));
+const About = React.lazy(() => import('./pages/About'));
+const Menu = React.lazy(() => import('./pages/Menu'));
+const Gallery = React.lazy(() => import('./pages/Gallery'));
 const Reservation = React.lazy(() => import('./pages/Reservation'));
-const Contact    = React.lazy(() => import('./pages/Contact'));
+const Contact = React.lazy(() => import('./pages/Contact'));
 const AdminLogin = React.lazy(() => import('./pages/AdminLogin'));
-const Admin      = React.lazy(() => import('./pages/Admin'));
-const NotFound   = React.lazy(() => import('./pages/NotFound'));
+const Admin = React.lazy(() => import('./pages/Admin'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -26,21 +26,21 @@ function AnimatedRoutes() {
       <Routes location={location} key={location.pathname}>
         {/* Public pages with Navbar + Footer */}
         <Route element={<MainLayout />}>
-          <Route path="/"           element={<Home />} />
-          <Route path="/about"      element={<About />} />
-          <Route path="/menu"       element={<Menu />} />
-          <Route path="/gallery"    element={<Gallery />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="/reservation" element={<Reservation />} />
-          <Route path="/contact"    element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
         </Route>
 
         {/* Admin routes */}
-        <Route path="/admin/login"  element={<AdminLogin />} />
-        <Route path="/admin"        element={<AdminLayout />}>
-          <Route index              element={<Admin />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Admin />} />
         </Route>
 
-        <Route path="*"             element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </AnimatePresence>
   );
